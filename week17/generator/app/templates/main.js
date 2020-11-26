21525182f563914545ec918e600cabe006ac456f
yuwenxia228 <65989973+yuwenxia228@users.noreply.github.com>
2020-11-26 18:28:46 +0800

    Add files via upload

---
+++ week17/generator/app/templates/main.js
@@ -0,0 +1,8 @@
+import HelloWorld from './HelloWorld.vue';
+
+import Vue from "vue";
+
+new Vue({
+  el: "#app",
+  render: h => h(HelloWorld)
+});
---
+++ week17/generator/app/templates/webpack.config.js
@@ -0,0 +1,29 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+var VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    module: {
+        rules: [
+            { test: /\.vue$/, use: 'vue-loader' },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // new HtmlWebpackPlugin({template: './src/index.html'})
+        new VueLoaderPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/*.html', to: '[name].[ext]' }
+            ]
+        })
+    ]
+};
